d55d4355c3b60ee21bb6e46a91ce594fa59cec56
druidmaciek <maciej@janowski.dev>
2021-05-09 21:25:58 +0200

    fix route

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,7 +3,7 @@
     <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
       <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
         <div class="px-5 py-2">
-          <a href="/tips" class="text-base text-gray-500 hover:text-gray-900">
+          <a href="/articles" class="text-base text-gray-500 hover:text-gray-900">
             Growth Tips
           </a>
         </div>
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -2,7 +2,7 @@
 	<div class="sm:flex justify-between items-center">
 		<a href="/" class="font-light tracking-widest text-white text-xl">SecureGrowthDigital</a>
 		<div class="flex justify-start sm:justify-end">
-			<a class="mr-4  hover:text-gray-100 hover:underline" href="/tips"
+			<a class="mr-4  hover:text-gray-100 hover:underline" href="/articles"
 				>Growth Tips for Accountants</a
 			>
 			<a class="hover:text-gray-100 hover:underline" href="/#contact">Contact Me</a>
