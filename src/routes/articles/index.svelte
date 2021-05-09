bd2482ddfed81c79eca51db9104b0db0d3854272
druidmaciek <maciej@janowski.dev>
2021-05-09 21:32:15 +0200

    fix data loading

--- src/routes/articles/index.svelte
+++ src/routes/articles/index.svelte
@@ -1,23 +1,40 @@
+<script context="module">
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+    import client, { defaultRequestConfig as reqConfig } from '../../storyblokClient'
+ 
+	export async function load({ page, fetch, session, context }) {
+		//const url = 'http://api.plos.org/search?q=ht';//`/articles/${page.params.slug}.json`;
+		//const res = await fetch(url);
+        
+        const url = 'cdn/stories/?starts_with=articles/';
+        const res = await client.get(url)
+        //console.log(res.data.story);
+
+		if (res.data.stories) {
+            console.log(res.data.stories);
+			return {
+				props: {
+					stories: await res.data.stories
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load ${url}`)
+		};
+	}
+</script>
+
 <script>
 	import Layout from '../../components/Layout.svelte';
 	import Blogs from '../../components/blog/List.svelte';
 	import Navbar from '../../components/Navbar.svelte';
 	import H2 from '../../components/Heading.svelte';
 
-	import StoryblokClient from 'storyblok-js-client';
-	const client = new StoryblokClient({
-		accessToken: 'L5O0kfyjrvqD5B1nPyOVMgtt'
-	});
-
-	export let stories = [];
-	client
-		.get('cdn/stories/?starts_with=articles/')
-		.then((response) => {
-			stories = response.data.stories;
-		})
-		.catch((error) => {
-			console.log(error);
-		});
+	export let stories;
 </script>
 
 <Navbar />
