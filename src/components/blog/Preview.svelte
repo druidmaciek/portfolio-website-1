54d3d5586a4dc4187d21e1886f1d3156e1924f18
druidmaciek <maciej@janowski.dev>
2021-05-09 21:42:03 +0200

    descriptive link

--- src/components/blog/Preview.svelte
+++ src/components/blog/Preview.svelte
@@ -11,8 +11,9 @@
 		<div class="mt-4 sm:mt-0 sm:px-12">
 			<h3 class="text-xl ">{story.content.title}</h3>
 			<p class="mt-0.5 text-sm">{story.content.intro}</p>
-			<div class="mt-1 pr-24">
-				<a href="{story.full_slug}" class="underline">Read More</a>
+			<div class="mt-2 pr-24">
+				<p>Click here to <a href="{story.full_slug}" class="underline">Read More</a></p>
+				
 			</div>
 		</div>
 	</div>
